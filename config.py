6216697af66394bbe789ccec52f70978e3e17035
Albert <mail@albertskog.se>
2018-04-07 19:49:23 +0200

    Change to Home Assistant broker

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 CONFIG = {
     "alarm_time": (6, 0),
     "time_increment": 15,
-    "broker": "smarthome.local",
+    "broker": "hassio.local",
     "outgoing_topic": "alarm_clock",
     "incoming_topic": "alarm_clock/command",
 }
